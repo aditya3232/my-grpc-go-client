915ff6c0f14e8c44f3d42ab2cdec0a768046da1e
aditya <m.aditya3232@gmail.com>
2026-09-21 16:37:58 +0700

    my-grpc-go-client sudah bisa

---
+++ internal/adapter/hello/hello_adapter.go
@@ -0,0 +1,35 @@
+package hello
+
+import (
+	"context"
+	"log"
+
+	"github.com/aditya3232/my-grpc-go-client/internal/port"
+	"github.com/aditya3232/my-grpc-proto/protogen/go/hello"
+	"google.golang.org/grpc"
+)
+
+type HelloAdapter struct {
+	helloClient port.HelloClientPort
+}
+
+func NewHelloAdapter(conn *grpc.ClientConn) (*HelloAdapter, error) {
+	client := hello.NewHelloServiceClient(conn)
+
+	return &HelloAdapter{
+		helloClient: client,
+	}, nil
+}
+
+func (a *HelloAdapter) SayHello(ctx context.Context, name string) (*hello.HelloResponse, error) {
+	helloRequest := &hello.HelloRequest{
+		Name: name,
+	}
+
+	greet, err := a.helloClient.SayHello(ctx, helloRequest)
+	if err != nil {
+		log.Fatalln("Error on SayHello : ", err)
+	}
+
+	return greet, nil
+}
--- internal/port/hello_port.go
+++ internal/port/hello_port.go
@@ -1,2 +1,13 @@
 package port
 
+import (
+	"context"
+
+	"github.com/aditya3232/my-grpc-proto/protogen/go/hello"
+	"google.golang.org/grpc"
+)
+
+// hello service client proto
+type HelloClientPort interface {
+	SayHello(ctx context.Context, in *hello.HelloRequest, opts ...grpc.CallOption) (*hello.HelloResponse, error)
+}
