8463d93f41665eeca76a2f92d1d353deb7d1ed17
aditya <m.aditya3232@gmail.com>
2026-09-25 19:22:11 +0700

    feat(resiliency): resiliency - timeout

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"math/rand"
+	"time"
 
-	"github.com/aditya3232/my-grpc-go-client/internal/adapter/bank"
+	// "github.com/aditya3232/my-grpc-go-client/internal/adapter/bank"
+	// dbank "github.com/aditya3232/my-grpc-go-client/internal/application/domain/bank"
 	// "github.com/aditya3232/my-grpc-go-client/internal/adapter/hello"
-	dbank "github.com/aditya3232/my-grpc-go-client/internal/application/domain/bank"
+
+	"github.com/aditya3232/my-grpc-go-client/internal/adapter/resiliency"
+	dresl "github.com/aditya3232/my-grpc-go-client/internal/application/domain/resiliency"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -21,7 +23,7 @@ func main() {
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
-	conn, err := grpc.Dial("localhost:9090", opts...)
+	conn, err := grpc.NewClient("localhost:9090", opts...)
 	if err != nil {
 		log.Fatalln("Can not connect to gRPC server : ", err)
 	}
@@ -38,15 +40,26 @@ func main() {
 	// runSayHelloToEveryone(helloAdapter, []string{"invoker", "spectre", "juggernut", "muerta", "sven"})
 	// runSayHelloContinous(helloAdapter, []string{"anna", "bella", "carol", "diana", "emma"})
 
-	bankAdapter, err := bank.NewBankAdapter(conn)
+	// bankAdapter, err := bank.NewBankAdapter(conn)
+	// if err != nil {
+	// 	log.Fatalln("Can not create BankAdapter : ", err)
+	// }
+
+	// runGetCurrentBalance(bankAdapter, "7835697001")
+	// runFetchExchangeRates(bankAdapter, "USD", "IDR")
+	// runSummarizeTransactions(bankAdapter, "7835697001", 10)
+	// runTransferMultiple(bankAdapter, "7835697001", "7835697003", 10)
+
+	resiliencyAdapter, err := resiliency.NewResiliencyAdapter(conn)
+
 	if err != nil {
-		log.Fatalln("Can not create BankAdapter : ", err)
+		log.Fatalln("Can not create ResiliencyAdapter :", err)
 	}
 
-	runGetCurrentBalance(bankAdapter, "7835697001")
-	runFetchExchangeRates(bankAdapter, "USD", "IDR")
-	runSummarizeTransactions(bankAdapter, "7835697001", 10)
-	runTransferMultiple(bankAdapter, "7835697001", "7835697003", 10)
+	runUnaryResiliencyWithTimeout(resiliencyAdapter, 2, 8, []uint32{dresl.OK}, 5*time.Second)
+	runServerStreamingResiliencyWithTimeout(resiliencyAdapter, 0, 3, []uint32{dresl.OK}, 15*time.Second)
+	runClientStreamingResiliencyWithTimeout(resiliencyAdapter, 0, 3, []uint32{dresl.OK}, 10, 10*time.Second)
+	runBiDirectionalResiliencyWithTimeout(resiliencyAdapter, 0, 3, []uint32{dresl.OK}, 10, 10*time.Second)
 }
 
 // func runSayHello(adapter *hello.HelloAdapter, name string) {
@@ -70,54 +83,95 @@ func main() {
 // 	adapter.SayHelloContinous(context.Background(), names)
 // }
 
-func runGetCurrentBalance(adapter *bank.BankAdapter, acct string) {
-	bal, err := adapter.GetCurrentBalance(context.Background(), acct)
+// func runGetCurrentBalance(adapter *bank.BankAdapter, acct string) {
+// 	bal, err := adapter.GetCurrentBalance(context.Background(), acct)
+// 	if err != nil {
+// 		log.Fatalln("Failed to call GetCurrentBalance ", err)
+// 	}
+
+// 	log.Println(bal)
+// }
+
+// func runFetchExchangeRates(adapter *bank.BankAdapter, fromCur string, toCur string) {
+// 	adapter.FetchExchangeRates(context.Background(), fromCur, toCur)
+// }
+
+// func runSummarizeTransactions(adapter *bank.BankAdapter, acct string, numDummyTransactions int) {
+// 	var tx []dbank.Transaction
+
+// 	for i := 1; i <= numDummyTransactions; i++ {
+// 		ttype := dbank.TransactionTypeIn
+
+// 		if i%3 == 0 {
+// 			ttype = dbank.TransactionTypeOut
+// 		}
+
+// 		t := dbank.Transaction{
+// 			Amount:          float64(rand.Intn(500) + 10),
+// 			TransactionType: ttype,
+// 			Notes:           fmt.Sprintf("Dummy transaction %v", i),
+// 		}
+
+// 		tx = append(tx, t)
+// 	}
+
+// 	adapter.SummarizeTransactions(context.Background(), acct, tx)
+// }
+
+// func runTransferMultiple(adapter *bank.BankAdapter, fromAcct string, toAcct string, numDummyTransactions int) {
+// 	var trf []dbank.TransferTransaction
+
+// 	for i := 1; i <= numDummyTransactions; i++ {
+// 		tr := dbank.TransferTransaction{
+// 			FromAccountNumber: fromAcct,
+// 			ToAccountNumber:   toAcct,
+// 			Currency:          "USD",
+// 			Amount:            float64(rand.Intn(200) + 5),
+// 		}
+
+// 		trf = append(trf, tr)
+// 	}
+
+// 	adapter.TransferMultiple(context.Background(), trf)
+// }
+
+func runUnaryResiliencyWithTimeout(adapter *resiliency.ResiliencyAdapter, minDelaySecond int32,
+	maxDelaySecond int32, statusCodes []uint32, timeout time.Duration) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+
+	defer cancel()
+
+	res, err := adapter.UnaryResiliency(ctx, minDelaySecond, maxDelaySecond, statusCodes)
+
 	if err != nil {
-		log.Fatalln("Failed to call GetCurrentBalance ", err)
+		log.Fatalln("Failed to call UnaryResiliency :", err)
 	}
 
-	log.Println(bal)
+	log.Println(res.DummyString)
 }
 
-func runFetchExchangeRates(adapter *bank.BankAdapter, fromCur string, toCur string) {
-	adapter.FetchExchangeRates(context.Background(), fromCur, toCur)
+func runServerStreamingResiliencyWithTimeout(adapter *resiliency.ResiliencyAdapter,
+	minDelaySecond int32, maxDelaySecond int32, statusCodes []uint32, timeout time.Duration) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	adapter.ServerStreamingResiliency(ctx, minDelaySecond, maxDelaySecond, statusCodes)
 }
 
-func runSummarizeTransactions(adapter *bank.BankAdapter, acct string, numDummyTransactions int) {
-	var tx []dbank.Transaction
+func runClientStreamingResiliencyWithTimeout(adapter *resiliency.ResiliencyAdapter,
+	minDelaySecond int32, maxDelaySecond int32, statusCodes []uint32,
+	count int, timeout time.Duration) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
 
-	for i := 1; i <= numDummyTransactions; i++ {
-		ttype := dbank.TransactionTypeIn
-
-		if i%3 == 0 {
-			ttype = dbank.TransactionTypeOut
-		}
-
-		t := dbank.Transaction{
-			Amount:          float64(rand.Intn(500) + 10),
-			TransactionType: ttype,
-			Notes:           fmt.Sprintf("Dummy transaction %v", i),
-		}
-
-		tx = append(tx, t)
-	}
-
-	adapter.SummarizeTransactions(context.Background(), acct, tx)
+	adapter.ClientStreamingResiliency(ctx, minDelaySecond, maxDelaySecond, statusCodes, count)
 }
 
-func runTransferMultiple(adapter *bank.BankAdapter, fromAcct string, toAcct string, numDummyTransactions int) {
-	var trf []dbank.TransferTransaction
+func runBiDirectionalResiliencyWithTimeout(adapter *resiliency.ResiliencyAdapter,
+	minDelaySecond int32, maxDelaySecond int32, statusCodes []uint32,
+	count int, timeout time.Duration) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
 
-	for i := 1; i <= numDummyTransactions; i++ {
-		tr := dbank.TransferTransaction{
-			FromAccountNumber: fromAcct,
-			ToAccountNumber:   toAcct,
-			Currency:          "USD",
-			Amount:            float64(rand.Intn(200) + 5),
-		}
-
-		trf = append(trf, tr)
-	}
-
-	adapter.TransferMultiple(context.Background(), trf)
+	adapter.BiDirectionalResiliency(ctx, minDelaySecond, maxDelaySecond, statusCodes, count)
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/aditya3232/my-grpc-go-client
 go 1.26.4
 
 require (
-	github.com/aditya3232/my-grpc-proto v0.0.0-20260924073249-e2b1edceec3d
+	github.com/aditya3232/my-grpc-proto v0.0.0-20260925021541-8a5114be8f2d
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20260918162117-cecb64721679
 	google.golang.org/grpc v1.84.0
 )
